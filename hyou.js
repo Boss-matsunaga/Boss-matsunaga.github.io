f04f93343ce66df31c982262e83c3f03b2f6e7f6
Boss-matsunaga <32508729+Boss-matsunaga@users.noreply.github.com>
2017-10-16 17:20:43 +0900

    Add files via upload

---
+++ hyou.js
@@ -0,0 +1,25 @@
+function hyoji(num)
+{
+  if (num == 0)
+  {
+  	    //document.getElementById("disp").style.display="table";
+    //document.getElementById("utenji").style.display="none";
+    $(document).ready(function() {
+  $('#seitenji').fadeIn(500);
+  $('#utenji').hide();
+ //  $('#seitenji').fadeIn(500);
+	// $('#utenji').delay(500).fadeOut(500);
+	});
+  }
+  else
+  {
+    //document.getElementById("utenji").style.display="table";
+    //document.getElementById("seitenji").style.display="none";
+	$(document).ready(function() {
+  $('#utenji').fadeIn(500);
+  $('#seitenji').hide();
+  // $('#utenji').fadeIn(500);
+  // $('#seitenji').delay(500).fadeOut(500);
+	});
+  }
+}
